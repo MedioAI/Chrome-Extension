5f115c9f09a1505345518a217d7005cbd4a0c799
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2024-05-27 11:54:55 -0400

    Update injector.js to include search functionality for chat list

--- engine/injector.js
+++ engine/injector.js
@@ -21,7 +21,7 @@ const engine = {
       }, 1000);
     }
 
-    engine.clearDB();
+    // engine.clearDB();
   },
 
   clearDB: () => {
@@ -1730,7 +1730,7 @@ const engine = {
 
           const header = document.createElement("div");
           header.innerHTML = `<div class="flex items-center space-x-2 justify-between mb-2">
-          <input id="medioSearchChatList type="text" placeholder="Search..." class="w-full rounded border p-1 px-2" />
+          <input id="medioSearchChatList" type="text" placeholder="Search..." class="w-full rounded border p-1 px-2" />
           <div id="medioPagination" class="flex items-center" style="width: 400px">
             <button id="medioPrev" class="medioDisabled">Previous</button>
             <div data-current="1" data-max="${chats.length}" id="medioPageCount" class="w-full">
@@ -1777,6 +1777,92 @@ const engine = {
           });
 
           chatList.append(container);
+
+          const searchBox = document.querySelector("#medioSearchChatList");
+          searchBox.addEventListener("input", (e) => {
+            const search = e.target.value.toLowerCase();
+            const chats = engine.chatHistory;
+
+            if (search === "") {
+              container.innerHTML = "";
+              const firstChats = chats.slice(0, engine.perPage);
+              firstChats.reverse().forEach((chat) => {
+                const newChat = document.createElement("div");
+                newChat.setAttribute("data-id", chat.id);
+                newChat.innerHTML = `<div class="medioChatItem mb-2 flex items-center justify-between p-2 border rounded">
+      <div>
+        <h4 class="text-lg font-bold">${chat.name}</h4>
+        <p class="text-sm flex space-x-4 text-gray-400"><span>${new Date(
+          chat.created_at
+        ).toLocaleString()}</span> <span style="opacity: 0.5">${
+                  chat.song_title
+                }</span></p>
+      </div>
+      <button class="medioChatDelete" data-id="${
+        chat.id
+      }"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M216 48h-36V36a28 28 0 0 0-28-28h-48a28 28 0 0 0-28 28v12H40a12 12 0 0 0 0 24h4v136a20 20 0 0 0 20 20h128a20 20 0 0 0 20-20V72h4a12 12 0 0 0 0-24M100 36a4 4 0 0 1 4-4h48a4 4 0 0 1 4 4v12h-56Zm88 168H68V72h120Zm-72-100v64a12 12 0 0 1-24 0v-64a12 12 0 0 1 24 0m48 0v64a12 12 0 0 1-24 0v-64a12 12 0 0 1 24 0"></path></svg></button>
+    </div>`;
+                container.append(newChat);
+              });
+
+              document
+                .querySelector("#medioPageCount")
+                .setAttribute("data-current", "1");
+              document
+                .querySelector("#medioPageCount")
+                .setAttribute("data-max", chats.length);
+              document.querySelector(
+                "#medioPageCount"
+              ).innerHTML = `Page 1 of ${chats.length}`;
+
+              document
+                .querySelector("#medioPrev")
+                .classList.add("medioDisabled");
+              document
+                .querySelector("#medioNext")
+                .classList.remove("medioDisabled");
+
+              return;
+            }
+
+            const filteredChats = chats.filter((chat) => {
+              if (chat.name) {
+                return (
+                  chat.name.toLowerCase().includes(search) ||
+                  chat.song_title.toLowerCase().includes(search)
+                );
+              } else {
+                return chat.song_title.toLowerCase().includes(search);
+              }
+            });
+
+            container.innerHTML = "";
+
+            filteredChats.reverse().forEach((chat) => {
+              const newChat = document.createElement("div");
+              newChat.setAttribute("data-id", chat.id);
+              newChat.innerHTML = `<div class="medioChatItem mb-2 flex items-center justify-between p-2 border rounded">
+      <div>
+        <h4 class="text-lg font-bold">${chat.name}</h4>
+        <p class="text-sm flex space-x-4 text-gray-400"><span>${new Date(
+          chat.created_at
+        ).toLocaleString()}</span> <span style="opacity: 0.5">${
+                chat.song_title
+              }</span></p>
+      </div>
+      <button class="medioChatDelete" data-id="${
+        chat.id
+      }"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M216 48h-36V36a28 28 0 0 0-28-28h-48a28 28 0 0 0-28 28v12H40a12 12 0 0 0 0 24h4v136a20 20 0 0 0 20 20h128a20 20 0 0 0 20-20V72h4a12 12 0 0 0 0-24M100 36a4 4 0 0 1 4-4h48a4 4 0 0 1 4 4v12h-56Zm88 168H68V72h120Zm-72-100v64a12 12 0 0 1-24 0v-64a12 12 0 0 1 24 0m48 0v64a12 12 0 0 1-24 0v-64a12 12 0 0 1 24 0"></path></svg></button>
+    </div>`;
+              container.append(newChat);
+            });
+
+            document.querySelector("#medioPageCount").innerHTML =
+              "1 of 1 Pages";
+
+            document.querySelector("#medioPrev").classList.add("medioDisabled");
+            document.querySelector("#medioNext").classList.add("medioDisabled");
+          });
 
           const medioPrev = document.getElementById("medioPrev");
           medioPrev.addEventListener("click", async (e) => {
@@ -1828,7 +1914,6 @@ const engine = {
                 current + " of " + totalPages + " Pages";
 
               if (parseInt(current) === 1) {
-                console.log("HEY", parseInt(current));
                 document
                   .querySelector("#medioPrev")
                   .classList.add("medioDisabled");
