183fac14828b18ff6a9c60e03056196097760678
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2024-05-27 19:16:18 -0400

    Refactor UI button labels and links for MedioAI integration

--- engine/ui/pagination.js
+++ engine/ui/pagination.js
@@ -234,7 +234,7 @@ const paginationMedioAI = {
         <span style="opacity: 0.5">${item.song_title}</span></p>
       </div>
       <button class="medioChatDelete" data-id="${item.id}">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M216 48h-36V36a28 28 0 0 0-28-28h-48a28 28 0 0 0-28 28v12H40a12 12 0 0 0 0 24h4v136a20 20 0 0 0 20 20h128a20 20 0 0 0 20-20V72h4a12 12 0 0 0 0-24M100 36a4 4 0 0 1 4-4h48a4 4 0 0 1 4 4v12h-56Zm88 168H68V72h120Zm-72-100v64a12 12 0 0 1-24 0v-64a12 12 0 0 1 24 0m48 0v64a12 12 0 0 1-24 0v-64a12 12 0 0 1 24 0"></path></svg>
+        ${iconsMedioAI.trash}
       </button>
     </div>`
   },
