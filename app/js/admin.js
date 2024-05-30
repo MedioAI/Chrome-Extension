ec7a4dc5d3f9251ef9464cb23a7351dd862d93da
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2024-05-30 12:13:54 -0400

    chore: Add admin page and script for JSON conversion and cleaning

---
+++ app/js/admin.js
@@ -0,0 +1,38 @@
+/*
+ * © MedioAI.com - Wynter Jones (@AI.MASSIVE)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+const convertButton = document.getElementById('convert')
+convertButton.addEventListener('click', async e => {
+  const currentJSONFile = chrome.runtime.getURL('database/tagbuilder/genres.json')
+  const newJSONFile = chrome.runtime.getURL('database/admin.json')
+
+  const current = await fetch(currentJSONFile)
+    .then(response => response.json())
+    .then(data => {
+      return data
+    })
+
+  const data = await fetch(newJSONFile)
+    .then(response => response.json())
+    .then(data => {
+      return data
+    })
+
+  const cleaned = []
+  for (let i = 0; i < data.length; i++) {
+    cleaned.push(data[i].value)
+  }
+
+  const combinedArray = cleaned.concat(current)
+  const capitalized = combinedArray.map(word => {
+    return word.replace(/\b\w/g, l => l.toUpperCase())
+  })
+  const unique = capitalized.filter((v, i, a) => a.indexOf(v) === i)
+  console.log(unique)
+})
